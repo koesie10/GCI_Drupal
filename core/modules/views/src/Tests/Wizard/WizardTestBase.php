2a86d774f5dd6cff1cb733f30bc64d91c67265ea
Koen Vlaswinkel <koen@vlaswinkel.info>
2014-12-04 18:45:26 +0100

    Add Drupal 8.0.x

---
+++ core/modules/views/src/Tests/Wizard/WizardTestBase.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\views\Tests\Wizard\WizardTestBase.
+ */
+
+namespace Drupal\views\Tests\Wizard;
+
+use Drupal\views\Tests\ViewTestBase;
+
+/**
+ * Views UI wizard tests.
+ */
+abstract class WizardTestBase extends ViewTestBase {
+
+  /**
+   * Set to TRUE to strict check all configuration saved.
+   *
+   * @see \Drupal\Core\Config\Testing\ConfigSchemaChecker
+   *
+   * @var bool
+   */
+  protected $strictConfigSchema = TRUE;
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('node', 'views_ui', 'block', 'rest');
+
+  protected function setUp() {
+    parent::setUp();
+
+    // Create and log in a user with administer views permission.
+    $views_admin = $this->drupalCreateUser(array('administer views', 'administer blocks', 'bypass node access', 'access user profiles', 'view all revisions'));
+    $this->drupalLogin($views_admin);
+  }
+
+}
